a31972104d2d174d93f490dee3177f2cd2f6ffda
Andre Menezes <djdedemenezes@gmail.com>
2022-05-24 09:25:03 -0300

    added address to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_120924) do
+ActiveRecord::Schema.define(version: 2022_05_24_121923) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
